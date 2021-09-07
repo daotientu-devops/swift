7eeed48ab1d516b9b2e77cb0b433e8035ba9397a
DAO TIEN TU <daotientu@gmail.com>
2021-09-07 02:05:34 -0700

    - [Swift]: Basic syntax (Keywords: Class, deinit, Enum, extension, Func, import, Init, internal, Let, operator, private, protocol, public, static, struct, subscript, typealias, var, break, case, continue, default, do, else, fallthrough, for, if, in, return, switch, where, while, as, dynamicType, false, is, nil, self, Self, super, true, _COLUMN_, _FILE_, _FUNCTION_, _LINE_,  associativity, convenience, dynamic, didSet, final, get, infix, inout, lazy, left, mutating, none, nonmutating, optional, override, postfix, precedence, prefix, Protocol, required, right, set, Type, unowned, weak, willSet)

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+var str = "Hello, World!"
+// prints "Value one \n" Adds. \n as terminator and " " as separator by default
+print("Value one")
+print("Value one", "Value two", separator: " Next value ", terminator: " End")
