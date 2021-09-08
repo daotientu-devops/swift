edd1dcab3a4116eb2540cd84f1fa1fa65ed22ecf
DAO TIEN TU <daotientu@gmail.com>
2021-09-08 08:07:36 -0700

    - [Swift]: Optional type say either "there is a value, and it equals x" or "there isn't a value at all"

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,30 +1,39 @@
 import UIKit
-// Variables
-var _var = "Hello, Swift 4!"
-print(_var)
-var 你好 = "你好世界"
-print(你好)
-var varA1 = "Godzilla"
-var varB1 = 1000.00
-print("Value of \(varA1) is more than \(varB1) millions")
-// type annotation
-var varB2: Float = 3.14159
-print(varB2)
-// type alias
-typealias Feet = Int
-var distance: Feet = 100
-print(distance)
-// type inference
-// varA is inferred to be of type int
-var varA = 42
-print(varA)
-// varB is inferred to be of type Double
-var varB = 3.14159
-print(varB)
-// varC is inferred to be of type Double
-var varC = 3 + 0.14159
-print(varC)
-var str = "Hello, World!"
-// prints "Value one \n" Adds. \n as terminator and " " as separator by default
-print("Value one")
-print("Value one", "Value two", separator: " Next value ", terminator: " End")
+// Optional type
+// var perhapsStr: String? = nil
+var myString: String? = nil
+if myString != nil {
+    print(myString)
+} else {
+    print("myString has nil value")
+}
+// Forced unwrapping
+var myString_0:String?
+myString_0 = "Hello, Swift 4!"
+if myString_0 != nil {
+    print(myString_0!)
+} else {
+    print("myString has nil value")
+}
+// Automatic unwrapping
+var myString_1:String!
+myString_1 = "Hello, Swift 4!"
+if myString_1 != nil {
+    print(myString_1!)
+} else {
+    print("myString has nil value")
+}
+/**
+ Optional Binding
+ S:tatement
+ if let constantName = someOptional {
+    statements
+ }
+ */
+var myString_2: String?
+myString_2 = "Hello, Swift 4!"
+if let yourString = myString_2 {
+    print("Your string has - \(yourString)")
+} else {
+    print("Your string does not have a value")
+}
